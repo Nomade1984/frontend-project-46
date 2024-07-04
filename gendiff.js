cae26b589beb49a6d9e2c1471cb4ed077ea61f58
murat <nomad1984@rambler.ru>
2024-07-04 23:54:00 +0700

    the 2nd step

--- gendiff.js
+++ gendiff.js
@@ -4,4 +4,8 @@ import { program } from 'commander';
 
 program.name('gendiff').description('Compares two configuration files and shows a difference.').version('1.0.0');
 
+program.usage('[options] <filepath1> <filepath2>');
+
+program.option('-f, --format [type]', 'output format');
+
 program.parse();
