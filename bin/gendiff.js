dfa05c786114c458b6cb4f3edbc00d64927470a2
murat <nomad1984@rambler.ru>
2024-07-22 23:20:12 +0700

    the 4th step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,10 @@
 
 import { program } from 'commander';
 
+import path from 'path';
+
+import { cwd } from 'node:process';
+
 import compare from '../src/compare.js';
 
 program.name('gendiff').description('Compares two configuration files and shows a difference.').version('1.0.0');
@@ -14,8 +18,25 @@ program
   .argument('<fileOne>', 'integer argument')
   .argument('<fileTwo>', 'integer argument')
   .action((fileOne, fileTwo) => {
-    compare(fileOne);
-    compare(fileTwo);
+    let makeAddress1;
+    let makeAddress2;
+
+    const firstLetter1 = fileOne[0];
+    const firstLetter2 = fileTwo[0];
+
+    if (firstLetter1 === '/') {
+      makeAddress1 = `${cwd(fileOne)}${fileOne}`;
+    } else {
+      makeAddress1 = path.resolve(fileOne);
+    }
+
+    if (firstLetter2 === '/') {
+      makeAddress2 = `${cwd(fileTwo)}${fileTwo}`;
+    } else {
+      makeAddress2 = path.resolve(fileTwo);
+    }
+
+    compare(makeAddress1, makeAddress2);
   });
 
 program.parse();
--- src/compare.js
+++ src/compare.js
@@ -1,9 +1,77 @@
 import fs from 'fs';
 
-const compare = (address) => {
-  fs.readFile(address, 'utf8', function (error, data) {
-    console.log(data);
+import _ from 'lodash';
+
+const compare = (address1, address2) => {
+  const result1 = fs.readFileSync(address1, 'utf8', function (error, data) {
+    return data;
   });
+
+  const result2 = fs.readFileSync(address2, 'utf8', function (error, data) {
+    return data;
+  });
+
+  const obj1 = JSON.parse(result1);
+  const arrKey1 = Object.keys(obj1);
+
+  const obj2 = JSON.parse(result2);
+  const arrKey2 = Object.keys(obj2);
+
+  const arrDiff1 = _.difference(arrKey1, arrKey2);
+  const arrDiff2 = _.difference(arrKey2, arrKey1);
+  const arrFinal = [];
+
+  for (let i = 0; i < arrDiff1.length; i += 1) {
+    if (arrKey1.includes(arrDiff1[i])) {
+      const smallArr = [arrDiff1[i], obj1[arrDiff1[i]]];
+      arrFinal.push(smallArr);
+    }
+  }
+
+  for (let i = 0; i < arrDiff2.length; i += 1) {
+    if (arrKey2.includes(arrDiff2[i])) {
+      const smallArr = [arrDiff2[i], obj2[arrDiff2[i]]];
+      arrFinal.push(smallArr);
+    }
+  }
+
+  const arrInter = _.intersection(arrKey1, arrKey2);
+
+  for (let i = 0; i < arrInter.length; i += 1) {
+    if (obj1[arrInter[i]] === obj2[arrInter[i]]) {
+      const smallArr = [arrInter[i], obj1[arrInter[i]]];
+      arrFinal.push(smallArr);
+    } else {
+      const smallArr1 = [arrInter[i], obj1[arrInter[i]]];
+      arrFinal.push(smallArr1);
+      const smallArr2 = [arrInter[i], obj2[arrInter[i]]];
+      arrFinal.push(smallArr2);
+    }
+  }
+
+  const sortedArr = arrFinal.sort();
+
+  for (let i = 0; i < sortedArr.length; i += 1) {
+    if (arrDiff1.includes(sortedArr[i][0])) {
+      sortedArr[i][0] = `- ${sortedArr[i][0]}`;
+    } else if (arrDiff2.includes(sortedArr[i][0])) {
+      sortedArr[i][0] = `+ ${sortedArr[i][0]}`;
+    } else if (sortedArr[i][0] === sortedArr[i + 1][0]) {
+      sortedArr[i][1] = obj1[sortedArr[i][0]];
+      sortedArr[i][0] = `- ${sortedArr[i][0]}`;
+      sortedArr[i + 1][1] = obj2[sortedArr[i + 1][0]];
+      sortedArr[i + 1][0] = `+ ${sortedArr[i + 1][0]}`;
+    }
+  }
+
+  const obj3 = {};
+
+  for (let i = 0; i < sortedArr.length; i += 1) {
+    obj3[sortedArr[i][0]] = sortedArr[i][1];
+  }
+
+  const result = JSON.stringify(obj3);
+  console.log(result);
 };
 
 export default compare;
